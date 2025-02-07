991edc1e847045362ff7761cb985a1534ee5cb56
Fredrik Blomqvist <fredrik.blomqvist@gpsgate.com>
2025-02-07 16:48:06 +0100

    add some utils

---
+++ src/utils.ts
@@ -0,0 +1,45 @@
+/**
+ * talk to local dev API or prod
+ */
+export function isLocalMode(): boolean {
+  return new URLSearchParams(location.search).has('local');
+}
+
+export function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+// Extract filename from Content-Disposition header if available.
+export function filenameFromResponseHeader(
+  response: Response,
+  { argname = 'filename' } = {}
+): string | null {
+  let filename = null;
+  const disposition = response.headers.get('Content-Disposition');
+  if (disposition && disposition.includes(argname + '=')) {
+    // This regex matches both quoted and unquoted filenames.
+    const filenameMatch = disposition.match(
+      new RegExp(`${argname}\\*?=(?:UTF-8'')?"?([^;\r\n"]+)`, 'i')
+    );
+    if (filenameMatch && filenameMatch[1]) {
+      filename = filenameMatch[1].replace(/["']/g, '');
+    }
+  }
+  return filename;
+}
+
+export function triggerBlobDownload(blob: Blob, filename: string): void {
+  const blobUrl = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = blobUrl;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(blobUrl);
+}
