2f82581f71aa7661a8fd8ede3703d35c92ad86f9
Fredrik Blomqvist <fredrik.blomqvist@gpsgate.com>
2025-02-08 01:31:06 +0100

    disable caching, only keep listeners to "trick" browser to make app installable. (probably wrong to cache .tsx files too...)

--- public/sw.js
+++ public/sw.js
@@ -1,30 +1,43 @@
+/**
+ * For now just enough to make the app installable.
+ * app is so small cache is not needed I'd say and it just complicates debugging.
+ */
+
 const CACHE_NAME = 'gmaps-exporter-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/src/main.tsx',
-  '/src/App.tsx',
-  '/src/index.css'
-];
+
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/manifest.json',
+//   '/src/main.tsx',
+//   '/src/App.tsx',
+//   '/src/index.css'
+// ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+  // event.waitUntil(
+  //   caches.open(CACHE_NAME)
+  //     .then((cache) => cache.addAll(urlsToCache))
+  // );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.includes('/share-target')) {
-    event.respondWith(
-      Response.redirect('/?url=' + encodeURIComponent(event.request.url))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+  // ...
 });
+
+self.addEventListener(
+  'fetch',
+  (event) => {
+    // if (event.request.url.includes('/share-target')) {
+    //   event.respondWith(
+    //     Response.redirect('/?url=' + encodeURIComponent(event.request.url))
+    //   );
+    //   return;
+    // }
+    // event.respondWith(
+    //   caches.match(event.request)
+    //     .then((response) => response || fetch(event.request))
+    // );
+  },
+  { once: true }
+); // yes, act as a dummy for now!
